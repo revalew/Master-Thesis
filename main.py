889ca2820a14dabf1d881239fd8c766724c25c9b
Maks <makskisiel@gmail.com>
2025-03-10 00:10:46 +0100

    Quick fix to ignore certain type errors (those are not classified as errors in MicroPython); Added new words to dictionary

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # full demo with web control panel
 # combines multi core and multi tasking
 
-import uasyncio as asyncio
+import uasyncio as asyncio  # type: ignore
 
-import machine
+import machine # type: ignore
 import gc
 
-from classes import WiFiConnection#, RequestHandler
+from classes import WiFiConnection, RequestHandler
 
 gc.enable()
 gc.collect()
@@ -19,8 +19,8 @@ if not WiFiConnection.start_ap_mode():
 #     print(f"{prop}\n")
 
 async def main() -> None:
-    # handler = RequestHandler()
-    # asyncio.create_task(asyncio.start_server(handler.handle_request, "0.0.0.0", 80))
+    handler = RequestHandler()
+    asyncio.create_task(asyncio.start_server(handler.handle_request, "0.0.0.0", 80))  # type: ignore
     gc.collect()
 
     counter = 0
@@ -28,7 +28,7 @@ async def main() -> None:
         # if counter % 1000 == 0:
         if counter == 1000:
             gc.collect()
-            print(f"Allocated RAM: {gc.mem_alloc()}\nFree RAM: {gc.mem_free()}\n")
+            print(f"Allocated RAM: {gc.mem_alloc()}\nFree RAM: {gc.mem_free()}\n")  # type: ignore
             counter = 0
         counter += 1
         await asyncio.sleep(0)
